46aca1d5d5c1c426c9aa12fb586764750da24503
Lazaro piñeiro <al1609133@upy.edu.mx>
2019-07-09 03:21:03 -0500

    robot and controller done

---
+++ controllers/u2_exam/build/release/u2_exam.d
@@ -0,0 +1,8 @@
+build/release/u2_exam.o: u2_exam.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/position_sensor.h \
+ /usr/local/webots/include/controller/c/webots/keyboard.h
